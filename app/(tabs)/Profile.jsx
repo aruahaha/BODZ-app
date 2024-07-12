862b0e9e10ab5cc7ca314195c81f0098f344a37b
Karan <karandayani39@gmail.com>
2024-07-12 15:05:41 +0530

    started creating profile page

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { Image } from "native-base";
+import { supabase } from "../../lib/supabaseClient";
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    )
-}
+  const { colors } = useTheme();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      setUser(data?.session?.user?.user_metadata);
+    };
+    getUser();
+  }, []);
+  return (
+    <View>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+          headerTitle: "",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: colors.background },
+        }}
+      />
+      <View className="h-32 w-32 absolute z-10 top-14 right-4">
+        <Image
+          source={require("../../assets/images/defaultProfile.png")}
+          alt="pfp"
+          className="h-32"
+        />
+      </View>
+      <View className="bg-[#ffbe7a] h-2/5 px-6 pt-8 justify-center">
+        <Text className="text-black text-2xl font-bold">Hey, {user?.name}</Text>
+        <Text className="text-black text-md">{user?.email}</Text>
+      </View>
+    </View>
+  );
+};
 
-export default Profile
+export default Profile;
--- components/categoryCard.jsx
+++ components/categoryCard.jsx
@@ -1,44 +1,72 @@
 // components/CategoryCard.js
 
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { Link } from 'expo-router';
-import { useTheme } from '@react-navigation/native';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { Link } from "expo-router";
+import { useTheme } from "@react-navigation/native";
 
 const categories = [
-    { label: 'Smartphones', name: "Smartphones", imgUrl: "https://m.media-amazon.com/images/I/31KxpX7Xk7L._SL500_.jpg" },
-    { label: 'Laptops', name: "Laptops", imgUrl: "https://m.media-amazon.com/images/I/51+U6oOCx4L._SL500_.jpg" },
-    { label: 'Earphones', name: "In-Ear Headphones", imgUrl: "https://m.media-amazon.com/images/I/31riIuaxjmL._SL500_.jpg" },
-    { label: 'Smart Watches', name: "Smart Watches", imgUrl: "https://m.media-amazon.com/images/I/41Hr4Mn6pCL._SL500_.jpg" },
-    // Add more categories as needed
+  {
+    label: "Smartphones",
+    name: "Smartphones",
+    imgUrl: "https://m.media-amazon.com/images/I/31KxpX7Xk7L._SL500_.jpg",
+  },
+  {
+    label: "Laptops",
+    name: "Laptops",
+    imgUrl: "https://m.media-amazon.com/images/I/51+U6oOCx4L._SL500_.jpg",
+  },
+  {
+    label: "Earphones",
+    name: "In-Ear Headphones",
+    imgUrl: "https://m.media-amazon.com/images/I/31riIuaxjmL._SL500_.jpg",
+  },
+  {
+    label: "Smart Watches",
+    name: "Smart Watches",
+    imgUrl: "https://m.media-amazon.com/images/I/41Hr4Mn6pCL._SL500_.jpg",
+  },
+  // Add more categories as needed
 ];
 
 const CategoryCard = () => {
-    const { colors } = useTheme()
-    return (
-        <View>
-            <View className="mb-5">
-                <Text style={{ fontFamily: "Lexend", color: colors.text }} className="text-2xl">Category</Text>
+  const { colors } = useTheme();
+  return (
+    <View>
+      <View className="mb-5">
+        <Text
+          style={{ fontFamily: "Lexend", color: colors.text }}
+          className="text-2xl"
+        >
+          Category
+        </Text>
+      </View>
+      <View className="flex-row flex-wrap gap-2 justify-center mb-20">
+        {categories.map((category) => (
+          <Link
+            key={category.name}
+            href={{
+              pathname: "/pages/Category",
+              params: { category: JSON.stringify(category.name) },
+            }}
+          >
+            <View className="bg-white p-2 rounded-lg items-center shadow-xl shadow-slate-600">
+              <Image
+                source={{ uri: category?.imgUrl }}
+                className="h-32 w-[140px] mb-2"
+              />
+              <Text
+                className="text-[15px] w-32 text-center"
+                style={{ fontFamily: "Lexend" }}
+              >
+                {category.label}
+              </Text>
             </View>
-            <View className="flex-row flex-wrap gap-5 justify-center mb-20">
-                {categories.map((category) => (
-                    <Link
-                        key={category.name}
-                        href={{ pathname: '/pages/Category', params: { category: JSON.stringify(category.name) } }}
-                    >
-                        <View className="bg-white p-2 rounded-lg items-center shadow-xl shadow-slate-600">
-
-                            <Image
-                                source={{ uri: category?.imgUrl }}
-                                className="h-32 w-[140px] mb-2"
-                            />
-                            <Text className="text-[15px] w-32 text-center" style={{ fontFamily: "Lexend" }}>{category.label}</Text>
-                        </View>
-                    </Link>
-                ))}
-            </View>
-        </View>
-    );
+          </Link>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 export default CategoryCard;
